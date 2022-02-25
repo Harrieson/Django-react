16cde846c257d5b9e85220493b61975aecd82652
Harrieson <harriemburu@gmail.com>
2022-02-25 19:29:40 +0300

    Section 8

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 # from django.contrib import admin
-from django.urls import path, include
-from .views import RoomView
+from django.urls import path
+from .views import RoomView, CreateRoomView, GetRoom
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('room', RoomView.as_view())
+    path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view())
+
 ]
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,6 +3,7 @@ import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 
 import { BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom'
+import Room from "./Room";
 
 export default class HomePage extends Component{
     constructor(props) {
@@ -16,6 +17,7 @@ export default class HomePage extends Component{
                 <Route exact path='/'><p>This is the HomePage</p></Route>
                 <Route exact path='/join' component={RoomJoinPage} />
                 <Route exact path='/create' component={CreateRoomPage} />
+                <Route  path='/room/:roomCode' component={Room} />
             </Switch>
         </Router>
         );
